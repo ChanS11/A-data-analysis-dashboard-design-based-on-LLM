4ba55395fe5ad9b5eacb426fb273cdfc500e7695
ChanS11 <1178743671@qq.com>
2026-02-25 17:02:45 +0100

    Update data paths in config.py
    to protect the sensitive data

--- src/config.py
+++ src/config.py
@@ -3,5 +3,5 @@
 OPENAI_API_KEY = ""
 
 # Data Paths
-PROFILGRUPPEN_PATH = r"E:\llm\Profilgruppen Extrusions AB"
-SIBBHULTSVERKEN_PATH = r"E:\llm\Sibbhultsverken AB"
+PROFILGRUPPEN_PATH = r"E:\llm\YOUR DATA"
+SIBBHULTSVERKEN_PATH = r"E:\llm\"YOUR DATA."
